2e9d7595dec1ab28b4e9f524fc635cb085c5dcf6
madhavanthangaraj <madhavanthangaraj24@gmail.com>
2025-07-08 09:29:51 +0530

    studenttask commited

--- Day-2/SpringSecond/src/main/java/org/example/controllers/HelloWorldController.java
+++ Day-2/SpringSecond/src/main/java/org/example/controllers/HelloWorldController.java
@@ -12,27 +12,32 @@ public class HelloWorldController {
     @Autowired
     private HelloWorldService hws;
 
+
     @GetMapping("/")
     public List<Employee> getMethod(){
         return hws.getMethod();
     }
+
+    @GetMapping("/{empId}")
+    public Employee getEmployeeById(@PathVariable  int empId){
+        return hws.getEmployeeById(empId);
+    }
+
     @PostMapping("/")
-    public List<Employee> postMethod(@RequestBody Employee employee) {
-        return hws.postMethod(employee);
+    public String postMethod(@RequestBody Employee emp) {
+//        public List<Employee> this will pass the emp list to service;s post method
+        return hws.postMethod(emp);
+    }
+
+    @DeleteMapping("/{empId}")
+    public String deleteElementById(@PathVariable int empId){
+        return hws.deleteEmployeeById(empId);
+
     }
 
     @PutMapping("/{empId}")
-    public String putMethod(@PathVariable int empId, @RequestBody Employee employee) {
-        return hws.putMethod(empId, employee);
+    public String updateEmployee(@PathVariable int empId,@RequestBody Employee Updatedemp){
+        return hws.updateEmployee(empId,Updatedemp);
     }
-
-
-    @DeleteMapping("/{empId}")
-    public List<Employee> deleteMethod(@PathVariable int empId) {
-        return hws.deleteMethod(empId);
-    }
-
 
 }
-
-
--- Day-2/SpringSecond/src/main/java/org/example/services/HelloWorldService.java
+++ Day-2/SpringSecond/src/main/java/org/example/services/HelloWorldService.java
@@ -2,6 +2,10 @@ package org.example.services;
 
 import org.example.models.Employee;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -9,40 +13,81 @@ import java.util.List;
 
 @Service
 public class HelloWorldService {
+
     List<Employee> emp = new ArrayList<>(
             Arrays.asList(
                     new Employee(1,"prasanth","Trainner"),
                     new Employee(2,"Yuvaraj","Faculty"))
     );
 
-
-    public List<Employee> getMethod() {
-        return emp;
-    }
-    public String HelloWorld(){
-        return "Hello Wordld";
-    }
-    public List<Employee> postMethod(Employee newEmployee) {
-        emp.add(newEmployee);
+    public List<Employee> getMethod(){
         return emp;
     }
 
-    public String putMethod(int empId, Employee updatedEmployee) {
-        for (Employee empObj : emp) {
-            if (empObj.getEmpId() == empId) {
-                empObj.setName(updatedEmployee.getName());
-                empObj.setJob(updatedEmployee.getJob());
-                return "Employee updated successfully";
+    public Employee getEmployeeById(int empId){
+        int index = 0;
+        boolean flag = false;
+        for(int i=0;i<emp.size();i++){
+            if(empId == emp.get(i).getEmpId()){
+                System.out.println("Emp id : "+emp.get(i).getEmpId()+emp.get(i));
+                index = i;
+                flag = true;
             }
         }
-        return "Employee not found";
+        if(flag) return emp.get(index);
+        else return new Employee();
+    }
+    public String postMethod(Employee newEmployee) { //List<Employee>
+
+        emp.add(newEmployee);
+//      return emp; this will return the employee details as list
+        return "Employee added successfully";
+
+    }
+
+    @DeleteMapping("/{empId}")
+    public String deleteEmployeeById(@PathVariable int empId) {
+        int index = -1;
+        boolean flag = false;
+        for (int i = 0; i < emp.size(); i++) {
+            if (empId == emp.get(i).getEmpId()) {
+                System.out.println("Emp id: " + emp.get(i).getEmpId() + " " + emp.get(i));
+                index = i;
+                flag = true;
+                break;
+            }
+        }
+        if (flag) {
+            emp.remove(index);
+            return "Employee Deleted Successfully";
+        } else {
+            return "No Such Employees";
+        }
     }
 
 
-    public List<Employee> deleteMethod(int empId) {
-        emp.removeIf(e -> e.getEmpId() == empId);
-        return emp;
+
+    @PutMapping("/{empId}")
+    public String updateEmployee(int empId, Employee updatedEmp) {
+        int index = -1;
+        boolean flag = false;
+        for (int i = 0; i < emp.size(); i++) {
+            if (empId == emp.get(i).getEmpId()) {
+                System.out.println("Emp id: " + emp.get(i).getEmpId() + " " + emp.get(i));
+                index = i;
+                flag = true;
+                break;
+            }
+        }
+        if (flag) {
+            emp.set(index, updatedEmp);
+            return "Employee Updated Successfully";
+        } else {
+            return "No Such Employees";
+        }
     }
+
+
 
 
 }
